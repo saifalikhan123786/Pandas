136b2daddd0127e9c05ae5eb49614643ec3b0c32
Saif Ali Khan <saifalikhan8798403@gmail.com>
2026-06-11 16:52:46 +0500

    output of data frames

--- save.py
+++ save.py
@@ -2,8 +2,15 @@ import pandas as pd
 
 data={
     "Name":['saif','Ali','Khan','ahmad'],
-    "Age":[10,20,30],
-    "City":['Lahore','Sialkot','shadra']
+    "Age":[10,20,30,40],
+    "City":['Lahore','Sialkot','shadra','pindi']
 }
 
 df=pd.DataFrame(data)
+print(df)
+
+# df.to_csv("output.csv", index=False)
+df.to_excel("output.xlsx", index=False)
+
+
+
