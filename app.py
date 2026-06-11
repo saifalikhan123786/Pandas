0711d9f81828f24c4e04f8d9e358e4ba25d16f44
Saif Ali Khan <saifalikhan8798403@gmail.com>
2026-06-11 15:44:32 +0500

    sales data samples files

---
+++ app.py
@@ -0,0 +1,7 @@
+# import pandas as pd
+
+# # Read data from CSV file into a dataframe
+
+# df=pd.read_csv("sales_data_sample.csv", encoding="Latin1")
+df=pd.read_excel("SampleSuperStore.xlsx")
+print(df)
