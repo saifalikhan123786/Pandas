66f8f5adf0c345d64ea2a3d3d4af95bca1f587f7
Saif Ali Khan <saifalikhan8798403@gmail.com>
2026-06-12 15:26:08 +0500

    descriptive method

---
+++ describe.py
@@ -0,0 +1,19 @@
+#   sample data frames
+
+
+import pandas as pd
+
+data={
+    "Name": ['saif','ali','usman','adil','haris','asad','ahmad','raju'],
+    "Age":[12,23,56,78,90,67,66,34],
+    "Salary":[50000,60000,45000,52000,49000,70000,48000,58000],
+    "Performance Score":[85,90,78,92,88,95,80,89]
+}
+
+df=pd.DataFrame(data)
+
+print("Sample DataFrame")
+print(df)
+
+print("Descriptive Statistics")
+print(df.describe())
