d7947839c593c29230de07dea2a3dd2a3786ba5b
Saif Ali Khan <saifalikhan8798403@gmail.com>
2026-06-13 14:51:34 +0500

    interpolate 1

---
+++ inter.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+
+data={
+    "Name": ['saif','Ali','usman','adil','haris','asad','ahmad','raju'],
+    "Age":[12,None,56,78,90,67,66,34],
+    "Salary":[50000,None,45000,52000,49000,70000,48000,58000],
+    "Performance Score":[85,None,78,92,88,95,80,89]
+}
+
+df=pd.DataFrame(data)
+print(df)
+
+## Linear ploynomial time cubic next 
+
+df.interpolate(mmethod="linear", axis=0 , inplace=True)
