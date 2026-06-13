46daf542f0a3998fac710377306c5d0f23065e0f
Saif Ali Khan <saifalikhan8798403@gmail.com>
2026-06-13 13:58:26 +0500

    fillna method

---
+++ handle2.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+
+data={
+    "Name": ['saif',None,'usman','adil','haris','asad','ahmad','raju'],
+    "Age":[12,None,56,78,90,67,66,34],
+    "Salary":[50000,None,45000,52000,49000,70000,48000,58000],
+    "Performance Score":[85,None,78,92,88,95,80,89]
+}
+
+df=pd.DataFrame(data)
+print(df)
+
+## Fillna method to give the default values 
+
+df.fillna(0,inplace=True)
+print(df)
