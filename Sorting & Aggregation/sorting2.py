cc7a7d0814bd270476776ecaa87dd388aa33554b
Saif Ali Khan <saifalikhan8798403@gmail.com>
2026-06-13 16:06:56 +0500

    sorting2 data

---
+++ Sorting & Aggregation/sorting2.py
@@ -0,0 +1,14 @@
+
+import pandas as pd
+
+data={
+    "Name": ['Saif','Ali','khan'],
+    "Age":[28,34,22],
+    "Salary":[10000,20000,30000]
+}
+
+df=pd.DataFrame(data)
+
+df.sort_values(by=["Age","Salary"], ascending=[True, False], inplace=True)
+print("Sorted Age my Descending")
+print(df)
